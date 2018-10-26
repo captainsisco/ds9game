646f800926d891f6d9d3a8c8919e47dd4d47086f
captainsisco <drg4176@gmail.com>
2018-10-26 15:50:35 -0200

    jogo com falas funcionando

--- jogo.js
+++ jogo.js
@@ -1,3 +1,12 @@
+var odo1= document.querySelector("audio[src='odo1.mp3']");
+var na_tela= document.querySelector("audio[src='natela.mp3']");
+var foster1= document.querySelector("audio[src='foster1.mp3']");
+var obrien= document.querySelector("audio[src='obrien1.mp3']");
+var sisko_coordenadas= document.querySelector("audio[src='siskocoordenadas.mp3']");
+var odocon= document.querySelector("audio[src='odoconfirm.mp3']");
+var odoklingons= document.querySelector("audio[src='odoklingons.mp3']");
+var red_alert= document.querySelector("audio[src='redalert.mp3']");
+var pr= document.querySelector("audio[src='phasersready.mp3']");
 var sisko_diario= document.querySelector("audio[src='siskodiario.mp3']");
 document.addEventListener("keypress",function(e){
 if(e.key=="Enter"){
@@ -5,7 +14,7 @@ sisko_diario.play();
 }
 if(e.key=="p"){
 var phaser= document.getElementById("phaser");
-	phaser.play();
+phaser.play();
 }
 if(e.key=="t"){
 var photon= document.getElementById("photon");
@@ -14,5 +23,23 @@ photon.play();
 if(e.key=="q"){
 var quantum= document.getElementById("quantum");
 quantum.play();
-}
+}})
+
+sisko_diario.addEventListener("ended",function(e){
+odo1.play();
 })
+odo1.addEventListener("ended",function(e){
+na_tela.play();
+})
+na_tela.addEventListener("ended",function(e){
+foster1.play();
+})
+foster1.addEventListener("ended",function(e){
+obrien.play();
+})
+obrien.addEventListener("ended",function(e){
+sisko_coordenadas.play();
+})
+sisko_coordenadas.addEventListener("ended",function(e){
+odocon.play();
+})	
